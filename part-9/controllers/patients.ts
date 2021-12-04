c4a083457bcc796f4f73889e256d230244527312
hfmckenna <hfmckenna@outlook.com>
2021-12-04 17:56:38 +0000

    9.23 Complete

--- part-9/controllers/patients.ts
+++ part-9/controllers/patients.ts
@@ -19,6 +19,52 @@ patientsRouter.get("/:id", (req: Request, res: Response) => {
   res.json(patient);
 });
 
+patientsRouter.post("/:id/entries", (req: Request, res: Response) => {
+  const patientIndex = patients.findIndex((patientIndexed) => patientIndexed.id === req.params.id);
+  const patient = patients.find((patient) => patient.id === req.params.id);
+  if (patient !== undefined) {
+    const { description, date, specialist, type } = req.body;
+
+    const diagnosisCodes = req.body?.diagnosisCodes ?? null;
+
+    let newEntry = Object.assign(
+      { id: uuid(), description, date, specialist, type },
+      diagnosisCodes && { diagnosisCodes }
+    );
+
+    let addEntry;
+
+    switch (type) {
+      case "Hospital":
+        const { discharge } = req.body;
+        addEntry = Object.assign(newEntry, discharge && { discharge });
+        break;
+      case "OccupationalHealthcare":
+        const { employerName } = req.body;
+        const sickLeave = req.body?.sickLeave ?? null;
+        addEntry = Object.assign(
+          newEntry,
+          employerName && { employerName },
+          sickLeave && { sickLeave }
+        );
+        break;
+      case "HealthCheck":
+        const { healthCheckRating } = req.body;
+        addEntry = Object.assign(
+          newEntry,
+          healthCheckRating && { healthCheckRating }
+        );
+        break;
+    }
+
+    patient.entries.push(addEntry);
+    patients.splice(patientIndex, 1, patient);
+    res.json(patient);
+  } else {
+    res.status(304);
+  }
+});
+
 patientsRouter.post("/", (req: Request, res: Response) => {
   const { name, dateOfBirth, ssn, gender, occupation } = req.body;
   const patient: Patient = {
@@ -28,7 +74,7 @@ patientsRouter.post("/", (req: Request, res: Response) => {
     ssn,
     gender,
     occupation,
-    entries: []
+    entries: [],
   };
 
   patients.push(patient);
